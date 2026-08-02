4a84090c4bd43b608e82e507d41e62214291e2e1
albyyy <albyantony25@gmail.com>
2026-08-02 23:38:16 +0530

    fix: correct Spring Web imports in SimulatorController

--- src/main/java/com/logplatform/controller/SimulatorController.java
+++ src/main/java/com/logplatform/controller/SimulatorController.java
@@ -2,8 +2,8 @@ package com.logplatform.controller;
 
 import com.logplatform.service.LogSimulatorService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.RequestMapping;
-import org.springframework.web.bind.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
